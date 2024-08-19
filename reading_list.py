fe779338583ac9092565c317303c8ba5e9d59e16
aloantony <aloantony@gmail.com>
2024-08-19 15:29:27 +0200

    Corrección mayúscula.

--- reading_list.py
+++ reading_list.py
@@ -1,7 +1,7 @@
 books = ['Zero to one', ' The Lean Startup', 'The  Mom Test', 'Make It Stick', 'Life in Code']
 
 books.append('Zero to Sold')
-books.remove('Zero to One')
+books.remove('Zero to one')
 books.pop(1)
 
 print(books)
